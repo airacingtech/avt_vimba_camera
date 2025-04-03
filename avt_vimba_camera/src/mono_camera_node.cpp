50e1c0b226470d48f7c29e75edfb419509a58808
Moises Lopez <mdlopezme@gmail.com>
2025-04-03 12:34:59 -0400

    commented out publish by accident....

--- avt_vimba_camera/src/mono_camera_node.cpp
+++ avt_vimba_camera/src/mono_camera_node.cpp
@@ -156,7 +156,7 @@ void MonoCameraNode::publishImagePtr(sensor_msgs::msg::Image & image) {
     // ss << "0x" << std::hex << reinterpret_cast<std::uintptr_t>(msg.get());
     // RCLCPP_INFO(this->get_logger(), "Published message with address: %s", 
     //             ss.str().c_str());
-    // pub_ptr->publish(std::move(msg));
+    pub_ptr->publish(std::move(msg));
 }
 
 void MonoCameraNode::startSrvCallback(const std::shared_ptr<rmw_request_id_t> request_header,

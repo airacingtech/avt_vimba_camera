acd2016edd1eb1f490afc1ee97dd0261ab05bdc6
kevshin2002 <kevshin2002@gmail.com>
2025-10-15 14:21:53 -1000

    nitros

--- avt_vimba_camera/src/mono_camera_node.cpp
+++ avt_vimba_camera/src/mono_camera_node.cpp
@@ -115,42 +115,6 @@ void MonoCameraNode::frameCallback(const FramePtr& vimba_frame_ptr)
 
   rclcpp::Time ros_time = this->get_clock()->now();
 
-<<<<<<< Updated upstream
-  // getNumSubscribers() is not yet supported in Foxy, will be supported in later versions
-  // if (camera_info_pub_.getNumSubscribers() > 0)
-  {
-    sensor_msgs::msg::Image img;
-    sensor_msgs::msg::CompressedImage compressed_image;
-    if (api_.frameToImage(vimba_frame_ptr, img, compressed_image, publish_compressed_))
-    {
-      sensor_msgs::msg::CameraInfo ci = cam_.getCameraInfo();
-      // Note: getCameraInfo() doesn't fill in header frame_id or stamp
-      ci.header.frame_id = frame_id_;
-      if (use_measurement_time_)
-      {
-        VmbUint64_t frame_timestamp;
-        vimba_frame_ptr->GetTimestamp(frame_timestamp);
-        ci.header.stamp = rclcpp::Time(frame_timestamp);
-      }
-      else
-      {
-        ci.header.stamp = ros_time;
-      }
-      img.header.frame_id = ci.header.frame_id;
-      img.header.stamp = ci.header.stamp;
-      camera_info_pub_.publish(img, ci);
-
-      if (publish_compressed_) {
-        compressed_image.header.frame_id = ci.header.frame_id;
-        compressed_image.header.stamp = ci.header.stamp;
-        compressed_pub->publish(compressed_image);
-      }
-    }
-    else
-    {
-      RCLCPP_WARN_STREAM(this->get_logger(), "Function frameToImage returned 0. No image published.");
-    }
-=======
   sensor_msgs::msg::Image img;
   sensor_msgs::msg::CompressedImage compressed_image;
   if (!api_.frameToImage(vimba_frame_ptr, img, compressed_image, publish_compressed_)) {
@@ -200,7 +164,6 @@ void MonoCameraNode::frameCallback(const FramePtr& vimba_frame_ptr)
   if (publish_compressed_) {
     compressed_image.header = ci.header;
     compressed_pub->publish(compressed_image);
->>>>>>> Stashed changes
   }
 }
 
